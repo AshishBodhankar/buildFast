e85a74c818bc93a1087ffeb6884ba8fc5df050fb
AshishBodhankar <ashishbodhankar1993@gmail.com>
2025-04-20 22:36:14 -0400

    new changes

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,36 @@
-from fastapi import FastAPI
-from . import schemas
+from typing import Annotated
+from fastapi import FastAPI, Depends, Query
+from .database import engine
+from .models import SQLModel, Blogs
+from sqlmodel import Session, select
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 
+def get_session():
+    with Session(engine) as session:
+        yield session
 
+SessionDep = Annotated[Session, Depends(get_session)]
 
-@app.post('/blog')
-def create(request: schemas.Blog):
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post('/blogs')
+def create(request: Blogs, session:SessionDep):
+    session.add(request)
+    session.commit()
+    session.refresh(request)
     return {'title':request.title,'body':request.body}
+
+@app.get('/blogs')
+def read(session:SessionDep,
+         offset: int = 0,
+         limit: Annotated[int, Query(le=100)] = 100,
+         ) -> list[Blogs]:
+    blogs = session.exec(select(Blogs).offset(offset).limit(limit)).all()
+    return blogs
 
